9efa6964a1385ed1ec8941480d562cdbea44377e
Manu Sridharan <msridhar@gmail.com>
2019-07-09 17:14:02 -0700

    temporary hack

--- do_like_javac/tools/check.py
+++ do_like_javac/tools/check.py
@@ -9,13 +9,14 @@ argparser = None
 def run(args, javac_commands, jars):
     # checker-framework javac.
     javacheck = os.environ['CHECKERFRAMEWORK']+"/checker/bin/javac"
-    checker_command = [javacheck, "-processor", args.checker, "-Astubs=" + str(args.stubs)]
+    checker_command = [javacheck, "-Astubs=" + str(args.stubs)]
 
     for jc in javac_commands:
         pprint.pformat(jc)
         javac_switches = jc['javac_switches']
+        pp = javac_switches['processorpath']
         cp = javac_switches['classpath']
-        cp = cp + ':' + args.lib_dir + ':'
+        cp = cp + ':' + pp + ':' + args.lib_dir + ':'
         java_files = jc['java_files']
         cmd = checker_command + ["-classpath", cp] + java_files
         common.run_cmd(cmd, args, 'check')
